2a2aa037c11af03b2cee7b9865f165b8cd7ffdd4
Mitch Olson <mitchjolson@gmail.com>
2019-07-28 20:05:10 -0500

    get details router complete

--- server/routes/genres.router.js
+++ server/routes/genres.router.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
 
 router.get('/:id', (req, res) => {
-    const sqlText = 'select * from movies;';
+    console.log('getting genres, req.params is', req.params)
+    const sqlText = `select genres.name from movies join junction on movies.id = junction.movie_id join genres on genres.id = junction.genre_id where movies.id = ${req.params.id};`;
     pool.query(sqlText)
         .then((response) => {
             res.send(response.rows)
